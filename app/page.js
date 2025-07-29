11b3ed2f7dc496192a2200c64062050439b433ca
Rishabh Singh <rrishabhssingh@gmail.com>
2025-07-29 10:59:36 +0530

    Updated page.js

--- app/page.js
+++ app/page.js
@@ -15,10 +15,10 @@ export default function Home() {
   return (
     <>
       <Hero />
-      <Stats />
       <HowItWorks />
-      <Features />
       <ClientTestimonials />
+      <Features />
+      <Stats />
       <VideoTestimonials />
       <CTA />
     </>
