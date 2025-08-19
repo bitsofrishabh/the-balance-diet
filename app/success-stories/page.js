14209b15dc7c41a674bffb19614ade3009f95758
Rishabh Singh <rrishabhssingh@gmail.com>
2025-08-19 07:36:44 +0530

    Updated page.js

--- app/success-stories/page.js
+++ app/success-stories/page.js
@@ -1,7 +1,7 @@
+'use client';
 
-
-import Link from 'next/link';
 import { useState } from 'react';
+import Link from 'next/link';
 import { Button } from '@/components/ui/button';
 import { Badge } from '@/components/ui/badge';
 import { 
