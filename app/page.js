cc91769a7959877b3b9c77bb5918bfa6de82a58f
bitsofrishabh <rrishabhssingh@gmail.com>
2025-09-06 17:54:58 +0530

    Comment out LocalContent component in Home function

--- app/page.js
+++ app/page.js
@@ -19,7 +19,7 @@ export default function Home() {
   return (
     <>
       <Hero />
-      <LocalContent />
+      {/* <LocalContent /> */}
       <HowItWorks />
       <ClientTestimonials />
       <WeightLossServices />
