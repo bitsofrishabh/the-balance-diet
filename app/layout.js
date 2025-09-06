f56a2a71bb05b8be9bc4ee4acd4e1fb344bb996a
bitsofrishabh <rrishabhssingh@gmail.com>
2025-09-06 17:14:11 +0530

    Fix gtag function syntax by removing unnecessary semicolon

--- app/layout.js
+++ app/layout.js
@@ -100,7 +100,7 @@ export default function RootLayout({ children }) {
         <script async src="https://www.googletagmanager.com/gtag/js?id=G-6FNNY5RCC4"></script>
         <script>
           window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
+          function gtag(){dataLayer.push(arguments)}
           gtag('js', new Date());
 
           gtag('config', 'G-6FNNY5RCC4');
