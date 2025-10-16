6b98ee4ad0c53e36ca852679dcc597d6baec2ba3
Rishabh Singh <rishabh@Rishabhs-MacBook-Air.local>
2025-10-16 08:34:45 +0530

    Refactor ProgramsPage layout to enhance user experience with new video section and updated core deliverables presentation

--- components/FacebookPixel.js
+++ components/FacebookPixel.js
@@ -1,56 +1,30 @@
-'use client'
+'use client';
 
-import { usePathname, useSearchParams } from 'next/navigation'
-import Script from 'next/script'
-import { useEffect } from 'react'
-import * as fpixel from '../lib/fpixel'
+import { usePathname } from 'next/navigation';
+import Script from 'next/script';
+import { useEffect, useState } from 'react';
+import * as fpixel from '@/lib/fpixel';
 
-const FacebookPixel = () => {
-  const pathname = usePathname()
-  const searchParams = useSearchParams()
+export const FacebookPixel = () => {
+    const [loaded, setLoaded] = useState(false);
+    const pathname = usePathname();
 
-  useEffect(() => {
-    if (!fpixel.FB_PIXEL_ID) {
-        console.warn("Facebook Pixel ID is not set. Tracking is disabled.");
-        return;
-    }
-    fpixel.pageview()
-  }, [pathname, searchParams])
+    useEffect(() => {
+        if (!loaded) return;
 
-  if (!fpixel.FB_PIXEL_ID) {
-    return null;
-  }
+        fpixel.pageview();
+    }, [pathname, loaded]);
 
-  return (
-    <>
-      <Script
-        id="fb-pixel-base"
-        strategy="afterInteractive"
-        dangerouslySetInnerHTML={{
-          __html: `
-            !function(f,b,e,v,n,t,s)
-            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-            n.queue=[];t=b.createElement(e);t.async=!0;
-            t.src=v;s=b.getElementsByTagName(e)[0];
-            s.parentNode.insertBefore(t,s)}(window, document,'script',
-            'https://connect.facebook.net/en_US/fbevents.js');
-            fbq('init', '${fpixel.FB_PIXEL_ID}');
-          `,
-        }}
-      />
-      <noscript>
-        <img
-          height="1"
-          width="1"
-          style={{ display: 'none' }}
-          src={`https://www.facebook.com/tr?id=${fpixel.FB_PIXEL_ID}&ev=PageView&noscript=1`}
-          alt=""
-        />
-      </noscript>
-    </>
-  )
-}
+    return (
+        <div>
+            <Script
+                id="fb-pixel"
+                src="/scripts/pixel.js"
+                strategy="afterInteractive"
+                onLoad={() => setLoaded(true)}
+                data-pixel-id={fpixel.FB_PIXEL_ID}
+            />
+        </div>
+    );
+};
 
-export default FacebookPixel
